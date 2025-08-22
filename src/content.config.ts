4166dee9c30228b87126f773e89eb0262575d59d
Diego <diego.rivera.estefano@gmail.com>
2025-08-22 13:19:51 -0400

    add some blogs(need fix). re design styles, add tags content, rewrite post content

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,18 +4,33 @@ import { defineCollection, z } from "astro:content";
 // 2. Import loader(s)
 import { glob, file } from "astro/loaders";
 
-// 3. Define your collection(s)
-// blogs posts collection
+const tags = defineCollection({
+  loader: file("src/data/tags.json"),
+  schema: z.object({
+    slug: z.string(),
+    name: z.string(),
+    color: z.string(),
+    description: z.string(),
+    seoTitle: z.string(),
+    metaDescription: z.string(),
+    keywords: z.string(),
+  }),
+});
+
 const posts = defineCollection({
   loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/data/posts" }),
   schema: z.object({
+    slug: z.string(),
     title: z.string(),
     description: z.string(),
     publishDate: z.date(),
     author: z.string(),
-    cover: z.string().optional(),
+    tags: z.array(z.string()).default([]),
+    readTime: z.string().default("5 min"),
+    image: z.string().optional(),
+    recommendedBlogs: z.array(z.string()).optional(),
   }),
 });
 
 // 4. Export a single `collections` object to register your collection(s)
-export const collections = { posts };
+export const collections = { tags, posts };
